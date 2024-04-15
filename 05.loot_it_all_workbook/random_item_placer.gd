4207506a4883c91e66cbba307420b18336cb29aa
Yujing14 <yuyu1@s.sfusd.edu>
2024-04-15 11:53:14 -0700

    L8 Completed

--- 05.loot_it_all_workbook/random_item_placer.gd
+++ 05.loot_it_all_workbook/random_item_placer.gd
@@ -7,15 +7,13 @@ var item_scenes:= [
 
 
 func _ready() -> void:
-	var gem_scene := preload("gem.tscn")
-	var gem_instance := gem_scene.instantiate()
-	add_child(gem_instance)
 	get_node("Timer").timeout.connect(_on_timer_timeout)
 
 func _on_timer_timeout() -> void:
 	var random_item_scene:PackedScene = item_scenes.pick_random()
 	var item_instance:= random_item_scene.instantiate()
 	add_child(item_instance)
+	
 	var viewport_size :=  get_viewport_rect().size
 	var random_position := Vector2(0, 0)
 	random_position.x = randf_range(0, viewport_size.x)
